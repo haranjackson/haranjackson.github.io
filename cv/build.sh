68aa4f8f41c67d9a3ebcba72ced478c6d076ae46
Haran Jackson <jackson.haran@gmail.com>
2020-03-22 15:23:19 +0000

    added version numbering to images

--- cv/build.sh
+++ cv/build.sh
@@ -3,3 +3,5 @@ xelatex -output-directory=tmp HaranJacksonCV.tex
 mv tmp/HaranJacksonCV.pdf ../assets/pdf
 rm -rf tmp
 convert -density 300 ../assets/pdf/HaranJacksonCV.pdf ../assets/img/HaranJacksonCV.png
+VERSION_NUMBER=$(date +%s)
+cat ../index0.html | sed -e "s/VERSION_NUMBER/$VERSION_NUMBER/g" >../index.html
